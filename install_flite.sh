652c6faee733065ed78201370a4ae234f45ff13d
Asma-Rehman-Ali <81525078+Asma-Rehman-Ali@users.noreply.github.com>
2021-03-28 21:27:32 +0500

    Update install_flite.sh

--- install_flite.sh
+++ install_flite.sh
@@ -1,7 +1,7 @@
-wget http://tts.speech.cs.cmu.edu/awb/flite-2.0.5-current.tar.bz2 -O - | tar xj
-mv flite-2.0.5-current flite
-cd flite && ./configure && make
-cd flite && sudo make install
-cd flite/testsuite && make lex_lookup
-sudo cp flite/testsuite/lex_lookup /usr/local/bin
+!wget http://tts.speech.cs.cmu.edu/awb/flite-2.0.5-current.tar.bz2 -O - | tar xj
+!mv flite-2.0.5-current flite
+!cd flite && ./configure && make
+!cd flite && sudo make install
+!cd flite/testsuite && make lex_lookup
+!sudo cp flite/testsuite/lex_lookup /usr/local/bin
 
